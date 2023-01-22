cbcb8b03b5ff27ff6399a3ac4079256910dee68e
Konstantin <qwerty58649@yandex.ru>
2023-01-22 12:06:38 +0300

    new skils

--- src/Components/FirstMessage/FirstMessage.tsx
+++ src/Components/FirstMessage/FirstMessage.tsx
@@ -1,30 +1,30 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
-import './message.css';
-import { printingText } from '../../printTextOnScreen';
-import myFoto1 from '../../img/myfoto2.jpg'
+import "./message.css";
+import { printingText } from "../../printTextOnScreen";
+import myFoto1 from "../../img/myfoto2.jpg";
 
-function FirstMessage(){
-    const [hello, setHello] = useState('');
-    const hi = `Hi! My name is Konstantin. I’m a web developer.
+function FirstMessage() {
+  const [hello, setHello] = useState("");
+  const hi = `Hi! My name is Konstantin. I’m a web developer.
     I have skills in: 
     HTML, React, redux, typescript, scss, css libraries, EsLint, testing the components.`;
-    useEffect(()=> {
-      printingText('hi-text',hi,hello, setHello);
-    }, [])
-    return (
-        <section id='start' className='wrapper-first flex z-10 p-5'>
-            <div className='start-media'>
-              <p className='text-white hi-text'>
-                  {hello}
-              </p>
-              <button className='button-cv'><a href='https://disk.yandex.ru/i/VJ1uUQcAWB1m6w'>DOWNLOAD CV</a></button>
-            </div>
-            <div className='wrapper-img'>
-              <img className='my-foto' alt='my foto' src={myFoto1}/>
-            </div>
-        </section>
-    )
+  useEffect(() => {
+    printingText("hi-text", hi, hello, setHello);
+  }, []);
+  return (
+    <section id="start" className="wrapper-first flex z-10 p-5">
+      <div className="start-media">
+        <p className="text-white hi-text">{hello}</p>
+        <button className="button-cv">
+          <a href="https://disk.yandex.ru/i/g51ADJX8rSWI4Q">DOWNLOAD CV</a>
+        </button>
+      </div>
+      <div className="wrapper-img">
+        <img className="my-foto" alt="my foto" src={myFoto1} />
+      </div>
+    </section>
+  );
 }
 
 export default FirstMessage;
